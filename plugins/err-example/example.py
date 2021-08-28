d451e460b500fbf6fcbac0e0e6a553a4147b282e
Larissa Campos <larissa.campos1207@gmail.com>
2021-08-28 18:09:37 +0000

    example.py

--- plugins/err-example/example.py
+++ plugins/err-example/example.py
@@ -1,11 +1,22 @@
-from errbot import BotPlugin, botcmd
+from errbot import BotPlugin, re_botcmd
+from random import randint
 
 
-class Example(BotPlugin):
+class Roleta(BotPlugin):
     """
-    This is a very basic plugin to try out your new installation and get you started.
-    Feel free to tweak me to experiment with Errbot.
-    You can find me in your init directory in the subdirectory plugins.
+    O intuito do chatbot é ser um jogo dinamico de sorte, 
+    no qual um unico individuo ou um grupo de pessoas 
+    possa jogar simultanemente.
+     A ideia do jogo é ser uma 'roleta russa' no qual cada 
+     jogador ira ativar a roleta e tentar a sorte em descobir
+    se sobrevive ou esta fora do jogo. Para ativar essa 
+    funcionalidade basta diditar 'roleta' que logoem seguida 
+    recebira uma respostar se você sobrevive ou não.
+
+Comandos de entreterimento: 
+BOM DIA - Bot responde bom dia + user 
+TESTE - Bot respnde TESTADO + user 
+ALO - Bot responde OLÁ + user
     """
 
     @botcmd  # flags a command
